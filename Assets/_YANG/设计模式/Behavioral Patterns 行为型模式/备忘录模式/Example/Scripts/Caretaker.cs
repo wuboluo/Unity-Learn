8886a3667b771bfcf083c67e0c2c3fa63f821b9a
wuweiyang <383049891@qq.com>
2023-02-14 18:13:22 +0800

    备忘录模式添加一条TODO

--- Assets/_YANG/设计模式/Behavioral Patterns 行为型模式/备忘录模式/Example/Scripts/Caretaker.cs
+++ Assets/_YANG/设计模式/Behavioral Patterns 行为型模式/备忘录模式/Example/Scripts/Caretaker.cs
@@ -2,6 +2,10 @@
 
 namespace Yang.DesignPattern.Memento.Example
 {
+    // 制作游戏存档，将 List ——> Dictionary，记录存档标签和数据快照
+    // 玩家通过选择标签进入不同存档
+    // TODO：查看【麦田物语】存档方式，看看是不是备忘录模式
+    
     public class Caretaker
     {
         private readonly List<Memento> _savedArticles = new();
