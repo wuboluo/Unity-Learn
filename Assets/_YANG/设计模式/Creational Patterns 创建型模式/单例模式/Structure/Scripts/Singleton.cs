820fe0fff94cb22c90344f945cae20bbe6410200
wuweiyang <383049891@qq.com>
2023-02-21 15:49:41 +0800

    单例模式 示例

---
+++ Assets/_YANG/设计模式/Creational Patterns 创建型模式/单例模式/Structure/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿namespace Yang.DesignPattern.Singleton.Structure
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        public string Info => "Singleton Class";
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            return _instance ??= new Singleton();
+        }
+    }
+}
---
+++ Assets/_YANG/设计模式/Creational Patterns 创建型模式/单例模式/Structure/Scripts/SingletonPatternStructure.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Yang.DesignPattern.Singleton.Structure
+{
+    public class SingletonPatternStructure : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log(Singleton.Instance().Info);
+        }
+    }
+}
